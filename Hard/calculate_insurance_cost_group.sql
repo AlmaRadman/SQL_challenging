3116435f62a679dac61c39443355c96a4033ff59
AlmaRadman <radman.alma99@gmail.com>
2025-10-26 16:25:46 +0330

    add calculate_insurance_cost_group.sql

---
+++ Hard/calculate_insurance_cost_group.sql
@@ -0,0 +1,22 @@
+/* 
+Each admission costs $50 for patients without 
+insurance, and $10 for patients with insurance.
+ All patients with an even patient_id have insurance.
+Give each patient a 'Yes' if they have insurance,
+ and a 'No' if they don't have insurance. 
+  */ 
+
+SELECT 
+  case
+      when patient_id%2 = 0
+        then 'Yes'
+      else 'No' 
+  end as has_insurance,
+sum(
+  case
+     when patient_id%2 = 0
+       then 10 
+     Else 50
+  end) as cost_after_insurance
+FROM admissions
+group by has_insurance;
