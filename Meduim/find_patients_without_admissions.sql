82c58c2e2fdac1d213270511049bcc03c0f89cda
AlmaRadman <radman.alma99@gmail.com>
2025-11-07 23:26:30 +0330

    add find_patients_without_admissions.sql

---
+++ Meduim/find_patients_without_admissions.sql
@@ -0,0 +1,18 @@
+/*
+Show patient_id, first_name, last_name from patients whose does not have any records in the admissions
+ table. (Their patient_id does not exist in any admissions.patient_id rows.)
+link: https://www.sql-practice.com/
+*/ 
+
+
+SELECT
+  p.patient_id,
+  p.first_name,
+  p.last_name
+FROM
+  patients p
+LEFT JOIN
+  admissions a 
+  ON p.patient_id = a.patient_id
+WHERE
+  a.patient_id IS NULL; 
